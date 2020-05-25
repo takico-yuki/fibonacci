92edb6cabaf06c4efcb20bc659b6ba2e31c071f2
takico <takita.y1026@gmail.com>
2020-05-26 00:49:01 +0900

    トリボナッチ数列関数作成

--- app.js
+++ app.js
@@ -2,22 +2,29 @@
 const memo = new Map();         //Mapの作成
 //フィボナッチ数列の初期値の設定
 memo.set(0,0);
-memo.set(1,1);
+memo.set(1,0);
+memo.set(2,1);
 /**
- * 
+ * トリボナッチ数列の値を返す関数
+ * 1.memoにトリボナッチ数列の値が存在する場合はその値を返す
+ * 2.memoに値が存在しない場合は値をセットする 
+ *      Map(n,トリボナッチ数)
  * @param {Number} 数字
- * @returns {Number} フィボナッチ数列
+ * @returns {Number} トリボナッチ数列
  * @author takico
  */
 function fib(n){
+    if(n < 0){
+        return ;
+    }
     if(memo.has(n)){
         //memoのkeyにnが存在していた場合
         return memo.get(n);
     }
     //一度計算したデータをmemoに追加する
-    const value = fib(n-1) + fib(n-2);
+    const value = fib(n-1) + fib(n-2) + fib(n-3);
     memo.set(n,value);
-    //フィボナッチを返す
+    //トリボナッチを返す
     return value
 }
 
